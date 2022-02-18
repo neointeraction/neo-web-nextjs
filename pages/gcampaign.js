a29f58603daf534a5eb215f0a7267e18ab0b450e
sg-Oranj <85916343+sg-Oranj@users.noreply.github.com>
2022-02-18 10:38:45 +0530

    ad conversio tag in head

--- pages/gcampaign.js
+++ pages/gcampaign.js
@@ -2,6 +2,7 @@ import { React, useState } from "react";
 import Head from "next/head";
 import ReactWOW from "react-wow";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import {Helmet} from "react-helmet";
 
 // import FormContact from "../components/FormContact";
 import ClientSlider from "../components/ClientSlider";
@@ -75,7 +76,18 @@ const gcampaign = () => {
               async
               src="https://js.convertflow.co/production/websites/35164.js"
             ></script> */}
+
+          <Helmet>
+           {/* Global site tag (gtag.js) - Google Ads: 1067948097 */}
+           <script async src="https://www.googletagmanager.com/gtag/js?id=AW-1067948097"></script>
+           <script> window.dataLayer = window.dataLayer || []; 
+             function gtag(){dataLayer.push(arguments)} 
+             gtag('js', new Date()); gtag('config', 'AW-1067948097'); </script>
+          </Helmet>
+           
+
       </Head>
+     
       <div className="container">
         <div className="home-content">
           <h1 className="main-title adPageTitle animated fadeIn delay-0.5s">
