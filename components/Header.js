6081c18e139fbd4c2a53b41379a3cb85bc98cfef
mshameer237 <mshameer237@gmail.com>
2022-07-29 23:23:40 +0530

    updated header component, added overlay nav menu
    updated header component, added overlay nav menu

--- components/Header.js
+++ components/Header.js
@@ -1,143 +1,245 @@
-import React, { Component } from "react";
-import Link from "next/link";
+import React, { useState } from "react";
+import Router from "next/router";
+import { useRouter } from "next/router";
+import { motion, AnimatePresence } from "framer-motion";
 
-// import Logo from "../images/LOGO.svg";
+import Menu from "../images/n-images/menu.svg";
+import Close from "../images/n-images/close.svg";
+import Facebook from "../images/facebook.svg";
+import LinkedIn from "../images/linkedIn.svg";
+import Twitter from "../images/twitter.svg";
+import Instagram from "../images/instagram.svg";
+import Dribble from "../images/dribbble.svg";
+import Behance from "../images/behance.svg";
+import Youtube from "../images/youtube.svg";
+import Medium from "../images/medium.svg";
 
-import Home from "../images/Home.svg";
-import Close from "../images/closeMenu.svg";
+const Header = () => {
+  const router = useRouter();
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleMenuOpen = () => {
+    setOpenMenu(true);
 
-export default class Header extends Component {
-  constructor() {
-    super();
-    this.state = {
-      menuActive: false,
-      activeClass: "",
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY >= 1) {
-        this.setState({ activeClass: "stick" });
-      } else if (window.scrollY === 0) {
-        this.setState({ activeClass: "" });
-      }
-    });
-  }
-
-  openMenu = (event) => {
-    event.stopPropagation();
-    this.setState({ menuActive: true });
-    let container = document.querySelector(".container");
-
-    let headWrapper = document.querySelector(".header-wrapper");
-    document.body.classList.add("scroll-hide");
-    container.classList.add("position-container");
-    headWrapper.classList.add("menu-overlay");
+    document.body.classList.add("menu-opened");
+  };
+  const handleMenuClose = () => {
+    setOpenMenu(false);
+    setTimeout(() => {
+      document.body.classList.remove("menu-opened");
+    }, 500);
   };
 
-  closeMenu = (event) => {
-    event.stopPropagation();
-    this.setState({ menuActive: false });
-    let container = document.querySelector(".container");
-    let headWrapper = document.querySelector(".header-wrapper");
-
-    document.body.classList.remove("scroll-hide");
-    container.classList.remove("position-container");
-    headWrapper.classList.remove("menu-overlay");
+  const slashMotion = {
+    rest: { x: 0, opacity: 0, ease: "easeOut", duration: 0.4, type: "tween" },
+    hover: {
+      opacity: 1,
+      x: 10,
+      transition: {
+        duration: 0.4,
+        type: "tween",
+        ease: "easeIn",
+      },
+    },
   };
 
-  render() {
-    return (
-      <div className="header-wrapper" onClick={this.closeMenu}>
-        <div className={`logo ${this.state.activeClass}`}>
-          {/* <img src={Logo} alt="logo" /> */}
-          <div className="logo-position">
-            <div className="logo-container ptop" onClick={this.openMenu}>
-              {this.state.menuActive === true && (
-                <div className="logo-container expand">
-                  <div className="logo-block block1 "></div>
-                  <div className="logo-block block2 "></div>
-                  <div className="logo-block block3 "></div>
-                  <div className="logo-block block4 "></div>
-                  <div className="menu-text" onClick={this.closeMenu}>
-                    <p>menu</p>
-                  </div>
-                  <ul className="menu-list">
-                    <li>
-                      <Link href="/">
-                        <span onClick={this.closeMenu} className="menu-link">
-                          <img src={Home} alt="home" />
-                        </span>
-                      </Link>
-                    </li>
+  const textMotion = {
+    rest: {
+      color: "grey",
 
-                    <li>
-                      <Link href="/UxService">
-                        <span onClick={this.closeMenu} className="menu-link">
-                          services
-                        </span>
-                      </Link>
+      transition: {
+        duration: 1,
+        type: "tween",
+        ease: "easeIn",
+      },
+    },
+    hover: {
+      color: "blue",
+
+      transition: {
+        duration: 0.4,
+        type: "tween",
+        ease: "easeOut",
+      },
+    },
+  };
+
+  const handleRoute = (path) => {
+    Router.push(path);
+    handleMenuClose();
+  };
+
+  return (
+    <div className="header-with-panel">
+      <div className="n-header-container">
+        <motion.div
+          className="n-menu"
+          onClick={handleMenuOpen}
+          initial="rest"
+          whileHover="hover"
+          animate="rest"
+        >
+          <motion.img src={Menu} alt="Menu" variants={textMotion} />
+          <motion.p variants={slashMotion} className="n-menu-text">
+            Menu
+          </motion.p>
+        </motion.div>
+        <div className="logo-container n-logo-container">
+          <div className="logo-container n-logo-container">
+            <div className="logo-block block1 blockflip1" />
+            <div className="logo-block block2 blockflip2" />
+            <div className="logo-block block3 blockflip3" />
+            <div className="logo-block block4 blockflip4" />
+            <motion.div
+              animate={{ opacity: [0, 1] }}
+              initial="hidden"
+              transition={{ delay: 0.5, duration: 0.5 }}
+              className="logo-text"
+            >
+              <div className="logo-text">
+                <p>neointeraction design</p>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+        <div className="n-header-contact">
+          <button class="custom-btn" onClick={() => handleRoute("/ContactUs")}>
+            Contact Us
+          </button>
+        </div>
+      </div>
+      <AnimatePresence>
+        {openMenu && (
+          <motion.div
+            initial="hidden"
+            animate={{ y: [-1000, 0], opacity: [0.5, 1] }}
+            exit={{ y: [0, -1000] }}
+            transition={{ duration: 0.4 }}
+            className={`nav-menu ${openMenu ? "open" : "close"}`}
+          >
+            <img
+              src={Close}
+              alt="Close"
+              onClick={handleMenuClose}
+              className="menu-close"
+            />
+            <motion.div
+              initial="hidden"
+              animate={{ opacity: [0, 1] }}
+              transition={{ delay: 0.5, duration: 0.5 }}
+              className="n-menu-content"
+            >
+              <div className="row ">
+                <div className="col-md-6">
+                  <ul className="n-menu-list">
+                    <li
+                      className={`n-menu-item ${
+                        router.pathname === "/" ? "active" : ""
+                      }`}
+                      onClick={() => handleRoute("/")}
+                    >
+                      Home
                     </li>
-                    <li>
-                      <Link href="/AboutUs">
-                        <span onClick={this.closeMenu} className="menu-link">
-                          about us
-                        </span>
-                      </Link>
+                    <li
+                      className={`n-menu-item ${
+                        router.pathname === "/AboutUs" ? "active" : ""
+                      }`}
+                      onClick={() => handleRoute("/AboutUs")}
+                    >
+                      About Us
                     </li>
-                    <li>
-                      <Link href="/Projects">
-                        <span onClick={this.closeMenu} className="menu-link">
-                          projects
-                        </span>
-                      </Link>
+                    <li
+                      className={`n-menu-item ${
+                        router.pathname === "/Projects" ? "active" : ""
+                      }`}
+                      onClick={() => handleRoute("/Projects")}
+                    >
+                      Projects
                     </li>
-                    <li>
-                      <Link href="/Career">
-                        <span onClick={this.closeMenu} className="menu-link">
-                          careers
-                        </span>
-                      </Link>
+                    <li
+                      className={`n-menu-item ${
+                        router.pathname === "/UxService" ||
+                        router.pathname === "/UiEngineering"
+                          ? "active"
+                          : ""
+                      }`}
+                      onClick={() => handleRoute("/UxService")}
+                    >
+                      Service
                     </li>
-                    <li>
-                      <Link href="/Blog">
-                        <span onClick={this.closeMenu} className="menu-link">
-                          blogs
-                        </span>
-                      </Link>
+                    <li
+                      className={`n-menu-item ${
+                        router.pathname === "/Career" ? "active" : ""
+                      }`}
+                      onClick={() => handleRoute("/Career")}
+                    >
+                      Career
                     </li>
-                    <li>
-                      <Link href="/ContactUs">
-                        <span onClick={this.closeMenu} className="menu-link">
-                          contact us
-                        </span>
-                      </Link>
-                    </li>
-                    <li onClick={this.closeMenu}>
-                      <img src={Close} alt="Close" />
+                    <li
+                      className={`n-menu-item ${
+                        router.pathname === "/Blog" ? "active" : ""
+                      }`}
+                      onClick={() => handleRoute("/Blog")}
+                    >
+                      Blog
                     </li>
                   </ul>
                 </div>
-              )}
-              {this.state.menuActive === false && (
-                <div className="logo-container">
-                  <div className="logo-block block1 blockflip1"></div>
-                  <div className="logo-block block2 blockflip2"></div>
-                  <div className="logo-block block3 blockflip3"></div>
-                  <div className="logo-block block4 blockflip4"></div>
-                  <div className="logo-text">
-                    <p>neointeraction design</p>
-                  </div>
-                  <div className="menu-text">
-                    <p>menu</p>
+                <div className="col-md-6 text-end">
+                  <div className="menu-right-content">
+                    <button
+                      class="custom-btn"
+                      onClick={() => handleRoute("/ContactUs")}
+                    >
+                      Contact Us
+                    </button>
+                    <ul className="social-icons n-social-icons no-bg">
+                      <li id="ln">
+                        <a
+                          href="https://www.linkedin.com/company/neointeraction-designs/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img src={LinkedIn} alt="LinkedIn" />
+                        </a>
+                      </li>
+
+                      <li id="insta">
+                        <a
+                          href="https://www.instagram.com/neointeraction/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img src={Instagram} alt="Instagram" />
+                        </a>
+                      </li>
+                      <li id="dribble">
+                        <a
+                          href="https://dribbble.com/neointeraction"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img src={Dribble} alt="Dribble" />
+                        </a>
+                      </li>
+                      <li id="behance">
+                        <a
+                          href="https://www.behance.net/neointeraction"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img src={Behance} alt="Behance" />
+                        </a>
+                      </li>
+                    </ul>
                   </div>
                 </div>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Header;
