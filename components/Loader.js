deb2d251540c14c09a441107d5482c7b7101e489
mshameer237 <mshameer237@gmail.com>
2022-09-01 00:07:37 +0530

    loader update
    loader update

--- components/Loader.js
+++ components/Loader.js
@@ -3,13 +3,21 @@ import React, { Component } from "react";
 export default class Loader extends Component {
   render() {
     return (
-      <div className="loader">
-        {/* <div class="loading"></div> */}
-        <div className="logo-container loader-logo">
-          <div className="logo-block block1 blockflip1"></div>
-          <div className="logo-block block2 blockflip2"></div>
-          <div className="logo-block block3 blockflip3"></div>
-          <div className="logo-block block4 blockflip4"></div>
+      // <div className="loader">
+      //   {/* <div class="loading"></div> */}
+      //   {/* <div className="logo-container loader-logo">
+      //     <div className="logo-block block1 blockflip1"></div>
+      //     <div className="logo-block block2 blockflip2"></div>
+      //     <div className="logo-block block3 blockflip3"></div>
+      //     <div className="logo-block block4 blockflip4"></div>
+      //   </div> */}
+
+      // </div>
+      <div className="ripple-loader-container">
+        <div class="ripple-loader">
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       </div>
     );
