59f1acc9f8f444dd72cea50b19de4ad600358135
Jishnu Krishnan <jishnukrishnan30@gmail.com>
2023-11-01 23:01:44 +0530

    added mobile styles

--- pages/diwali.js
+++ pages/diwali.js
@@ -76,7 +76,7 @@ const diwali = () => {
         </div>
       </section>
 
-      <section>
+      <section className="guidelines">
         <h2>Submission Guidelines:</h2>
         <ol>
           <li>
@@ -92,9 +92,9 @@ const diwali = () => {
             </p>
           </li>
         </ol>
-      </section>
 
-      <section className="pattern"></section>
+        <div className="pattern"></div>
+      </section>
     </div>
   );
 };
